f0d163d407e25642f30d8b017ff6fee6a18434df
Pavel Grushin <pavelPearFruit@gmail.com>
2023-11-26 23:39:27 +0300

    Settings Join Game

---
+++ Assets/Scripts/Multipleer/TestScript.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class TestScript : MonoBehaviour
+{
+    public void Map_1()
+    {
+        SceneManager.LoadScene("Map_1");
+    }
+}
