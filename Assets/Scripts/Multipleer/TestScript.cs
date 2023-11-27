e1357f31de9f260359a270e27f1e776bd3aea681
Pavel Grushin <pavelPearFruit@gmail.com>
2023-11-27 10:14:34 +0300

    Complite Test Multipleer

--- Assets/Scripts/Multipleer/TestScript.cs
+++ Assets/Scripts/Multipleer/TestScript.cs
@@ -1,12 +1,17 @@
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using Photon.Pun;
+using Photon.Realtime;
 
-
-public class TestScript : MonoBehaviour
+public class TestScript : MonoBehaviourPunCallbacks
 {
+    private void Start()
+    {
+        PhotonNetwork.AutomaticallySyncScene = true;
+    }
     public void Map_1()
     {
-        SceneManager.LoadScene("Map_1");
+        PhotonNetwork.LoadLevel("Map_1");
     }
 }
